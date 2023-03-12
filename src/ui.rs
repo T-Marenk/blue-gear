ad5f935b8ba28a542670e73b6873e0efdc6f2724
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-03-12 14:04:57 +0200

    Bluetooth status

--- src/ui.rs
+++ src/ui.rs
@@ -1,37 +1,36 @@
 use tui::{
     backend::Backend,
     Frame,
-    layout::{Layout, Constraint, Direction, Rect},
-    widgets::{Block, Borders, Table, Row, TableState},
-    style::{Style, Color}
+    layout::{Layout, Constraint, Rect},
+    widgets::{Block, Borders, Tabs},
+    style::{Style, Color},
+    text::{Span, Spans}
 };
 
 pub fn draw<B: Backend>(f: &mut Frame<B>, status: bool) {
     let constraints: Vec<Constraint> = vec![
-        Constraint::Percentage(10),
-        Constraint::Percentage(90)
+        Constraint::Length(3),
+        Constraint::Min(0)
     ];
     let parts = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(constraints)
+        .constraints(constraints.as_ref())
         .split(f.size());
     
     let status = match status {
         true => "On",
-        false => "False"
+        false => "Off"
     };
 
     draw_state_block(f, status, &parts);
 }
 
 fn draw_state_block<B: Backend>(f: &mut Frame<B>, status: &str, parts: &Vec<Rect>) {
-    let rows = vec![
-        Row::new(vec![""]),
-        Row::new(vec!["Bluetooth status", status]).style(Style::default().fg(Color::Green))
-    ];
-    let mut state = TableState::default();
-    let table = Table::new(rows)
-        .widths(&[Constraint::Length(16), Constraint::Length(5), Constraint::Length(5)])
-        .block(Block::default().title("Blue-rs").borders(Borders::ALL));
-    f.render_stateful_widget(table, parts[0], &mut state);
+    let titles = vec![
+        Spans::from("Bluetooth status"),
+        Spans::from(Span::styled(status, Style::default().fg(Color::Green)))];
+    let tabs = Tabs::new(titles)
+        .block(Block::default().borders(Borders::ALL).border_style(Style::default().fg(Color::Gray)).title("Blue-rs"))
+        .highlight_style(Style::default().fg(Color::Cyan))
+        .select(0);
+    f.render_widget(tabs, parts[0]);
 }
