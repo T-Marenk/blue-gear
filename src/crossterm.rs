8a61c51a48e0839ac9b59424aba34561a2b5f050
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-03-14 22:53:31 +0200

    key handeling

--- src/crossterm.rs
+++ src/crossterm.rs
@@ -7,7 +7,7 @@ use crossterm::{
     event::{self, Event, KeyCode},
     execute, 
     terminal::{EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen, enable_raw_mode}, 
-    event::{EnableMouseCapture, DisableMouseCapture}
+    event::{EnableMouseCapture, DisableMouseCapture, KeyEvent}
 };
 use futures::pin_mut; // Will be used later
 use tokio::runtime::Runtime;
@@ -24,7 +24,7 @@ use crate::{
 /// Start the application
 /// Creates the runtime, initializes App for usage and created alternate terminal for ui
 pub fn start() -> Result<(), Box<dyn Error>> {
-    let rt = match create_rt() {
+    let rt: Runtime = match create_rt() {
         Ok(rt) => rt,
         Err(e) => return Err(e)
     };
@@ -97,27 +97,40 @@ fn bluetooth_off<B: Backend>(
 ) -> Result<(), Box<dyn Error>> {
     drawer(&mut terminal, &app_mutex);
 
-    if crossterm::event::poll(Duration::from_millis(0))? {
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => {
-                    let mut app = app_mutex.lock().unwrap();
-                    app.state = 1;
-                }
-                _ => {},
-            }
+    loop {
+        let response: u8 = match event::read()? {
+            Event::Key(key) => handle_key(&app_mutex, &mut terminal, key),
+            _ => 0
+        };
+        if response == 1 {
+            break
         }
     }
 
     Ok(())
 }
 
+fn handle_key<B: Backend>(app_mutex: &Mutex<App>, mut terminal: &mut Terminal<B>, key: KeyEvent) -> u8 {
+        match key.code {
+                KeyCode::Char('q') => test(&app_mutex),
+                KeyCode::Char('d') => drawer(&mut terminal, &app_mutex),
+                _ => 0
+            }
+}
+
+fn test(app_mutex: &Mutex<App>) -> u8 {
+    let mut app = app_mutex.lock().unwrap();
+    app.state = 1;
+    1
+}
 /// Call draw function in order to render the ui with changes to the terminal
-fn drawer<B: Backend>(terminal: &mut Terminal<B>, app_mutex: &Mutex<App>) {
+fn drawer<B: Backend>(terminal: &mut Terminal<B>, app_mutex: &Mutex<App>) -> u8 {
     let mut app = match app_mutex.lock() {
         Ok(app) => app,
         Err(e) => e.into_inner()
     };
 
     terminal.draw(|f| draw(f, &mut app)).unwrap();
+
+    0
 }
